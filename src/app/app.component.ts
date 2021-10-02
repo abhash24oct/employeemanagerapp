756903dc0e1d85681c6c72c7e0da03467ae5fbc7
abahsh24oct <abahsh24oct@gmail.com>
2021-10-02 19:48:33 +0200

    Updated the template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,4 +27,24 @@ export class AppComponent implements OnInit {
       }
     );
   }
+
+  public onOpenModal(employee: Employee | null, mode: string): void {
+    const container = document.getElementById('main-container');
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+    if (mode === 'add') {
+      button.setAttribute('data-target', '#add-employee');
+    }
+    if (mode === 'edit') {
+      button.setAttribute('data-target', '#edit-employee');
+    }
+    if (mode === 'delete') {
+      button.setAttribute('data-target', '#delete-employee');
+    }
+
+    container?.appendChild(button);
+    button.click();
+  }
 }

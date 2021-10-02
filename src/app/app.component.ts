5f1a29f78a169bd90dcd00f7c92171cb93f5f786
abahsh24oct <abahsh24oct@gmail.com>
2021-10-02 22:48:59 +0200

    Edit and Add form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Employee } from './employee';
 import { EmployeeService } from './employee.service';
 
@@ -9,7 +10,8 @@ import { EmployeeService } from './employee.service';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  public employees: Employee[] = [];
+  public employees: Employee[];
+  public editEmployee: Employee;
 
   constructor(private employeeService: EmployeeService) {}
 
@@ -27,8 +29,30 @@ export class AppComponent implements OnInit {
       }
     );
   }
+  public onAddEmployee(addForm: NgForm): void {
+    document.getElementById('add-employee-form')?.click();
+    this.employeeService.addEmployee(addForm.value).subscribe(
+      (response: Employee) => {
+        console.log(response);
+      },
+      (error: HttpErrorResponse) => {
+        console.error(error);
+      }
+    );
+  }
 
-  public onOpenModal(employee: Employee | null, mode: string): void {
+  public onUpdateEmployee(employee: Employee): void {
+    this.employeeService.updateEmployee(employee).subscribe(
+      (response: Employee) => {
+        console.log(response);
+      },
+      (error: HttpErrorResponse) => {
+        console.error(error);
+      }
+    );
+  }
+
+  public onOpenModal(employee: Employee, mode: string): void {
     const container = document.getElementById('main-container');
     const button = document.createElement('button');
     button.type = 'button';
@@ -38,12 +62,12 @@ export class AppComponent implements OnInit {
       button.setAttribute('data-target', '#add-employee');
     }
     if (mode === 'edit') {
+      this.editEmployee = employee;
       button.setAttribute('data-target', '#edit-employee');
     }
     if (mode === 'delete') {
       button.setAttribute('data-target', '#delete-employee');
     }
-
     container?.appendChild(button);
     button.click();
   }
